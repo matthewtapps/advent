d1c6aba5a15c636cb32b3a04a6cf0c5eb9abbbfc
Matthew Tapps <mail@matthewtapps.com>
2024-12-03 21:10:49 +1100

    oh my god

--- day-three/src/main.rs
+++ day-three/src/main.rs
@@ -4,34 +4,72 @@ use std::{
     path::Path,
 };
 
-use regex::Regex;
+use fancy_regex::Regex;
 
 fn main() {
-    let lines = parse_file("input.txt");
+    let line = parse_file("input.txt");
     let mut total = 0;
-    for line in lines {
-        let digits = parse_instances_of_mul_in_string(line);
-        total += sum_products(&digits);
-    }
+    let digits = parse_instances_of_mul_in_string(&line);
+    total += sum_products(&digits);
+    println!("{}", total);
+
+    total = calculate_conditional_result(line);
     println!("{}", total)
 }
 
-// thanks stackoverflow
-fn parse_file(file_path: impl AsRef<Path>) -> Vec<String> {
-    let contents = File::open(file_path).expect("no such file");
-    let buf = BufReader::new(contents);
-    buf.lines()
-        .map(|line| line.expect("could not parse line"))
-        .collect()
+fn calculate_conditional_result(line: String) -> i32 {
+    let mut total = 0;
+    let mut valid = true;
+    let mul_instances = capture_mul_and_prefix(&line);
+    for (index, instance) in mul_instances.iter().enumerate() {
+        let dos_or_donts = capture_do_or_dont(&instance);
+        let last_prefix = last_prefix_is_do(&dos_or_donts);
+        if index == 0 {
+            if dos_or_donts.len() == 0 || last_prefix {
+                let digits = parse_instances_of_mul_in_string(&instance);
+                total += sum_products(&digits);
+            }
+        } else if valid {
+            if last_prefix {
+                let digits = parse_instances_of_mul_in_string(&instance);
+                total += sum_products(&digits);
+            } else {
+                valid = false
+            }
+        } else {
+            if last_prefix {
+                valid = true;
+                let digits = parse_instances_of_mul_in_string(&instance);
+                total += sum_products(&digits);
+            }
+        }
+    }
+    return total;
 }
 
-fn parse_instances_of_mul_in_string(string: String) -> Vec<(i32, i32)> {
+// thanks stackoverflow
+fn parse_file(file_path: impl AsRef<Path>) -> String {
+    let contents = File::open(file_path).expect("no such file");
+    let buf = BufReader::new(contents);
+    let mut full_string = "".to_string();
+    let lines: Vec<String> = buf
+        .lines()
+        .map(|line| line.expect("could not parse line"))
+        .collect();
+    for line in lines {
+        full_string.push_str(line.as_str())
+    }
+    return full_string;
+}
+
+fn parse_instances_of_mul_in_string(string: &String) -> Vec<(i32, i32)> {
     let pattern = Regex::new(r"(?:mul\((?<one>\d+),(?<two>\d+)\))").unwrap();
     let digits: Vec<(i32, i32)> = pattern
         .captures_iter(string.as_str())
         .map(|caps| {
-            let digit_one = caps.name("one").unwrap().as_str();
-            let digit_two = caps.name("two").unwrap().as_str();
+            let result = caps.unwrap();
+            let digit_one = result.name("one").unwrap().as_str();
+            let digit_two = result.name("two").unwrap().as_str();
             (
                 digit_one.parse::<i32>().unwrap(),
                 digit_two.parse::<i32>().unwrap(),
@@ -40,6 +78,47 @@ fn parse_instances_of_mul_in_string(string: String) -> Vec<(i32, i32)> {
         .collect();
 
     return digits;
+}
+
+fn capture_mul_and_prefix(string: &String) -> Vec<String> {
+    let pattern = Regex::new(r"(?<mul_with_prefix>\G.*?mul\(\d+,\d+\))+").unwrap();
+    let muls_with_prefixes: Vec<String> = pattern
+        .captures_iter(string.as_str())
+        .map(|caps| {
+            caps.unwrap()
+                .name("mul_with_prefix")
+                .unwrap()
+                .as_str()
+                .to_string()
+        })
+        .collect();
+    return muls_with_prefixes;
+}
+
+fn capture_do_or_dont(string: &String) -> Vec<String> {
+    let pattern = Regex::new(r"(?<do_or_dont>do\(\)|don't\(\))").unwrap();
+    let dos_or_donts = pattern
+        .captures_iter(string.as_str())
+        .map(|caps| {
+            caps.unwrap()
+                .name("do_or_dont")
+                .unwrap()
+                .as_str()
+                .to_string()
+        })
+        .collect();
+    return dos_or_donts;
+}
+
+fn last_prefix_is_do(strings: &Vec<String>) -> bool {
+    if strings.len() == 0 {
+        return false;
+    }
+    let last_prefix = strings.last().unwrap();
+    if last_prefix == &"do()".to_string() {
+        return true;
+    }
+    return false;
 }
 
 fn sum_products(digits: &Vec<(i32, i32)>) -> i32 {
@@ -58,24 +137,23 @@ mod tests {
     #[test]
     fn parse_file_test_one() {
         let result = parse_file("./mocks/mock1.txt");
-        let want = vec!["testfilewithonelineshouldhaveonelineoutput".to_string()];
+        let want = "testfilewithonelineshouldhaveonelineoutput".to_string();
         assert_eq!(want, result);
     }
 
     #[test]
     fn parse_file_test_two() {
         let result = parse_file("./mocks/mock2.txt");
-        let want = vec![
-            "testfilewithonelineshouldhaveonelineoutput".to_string(),
-            "nowthereisanotherlinesotwolinesoutput".to_string(),
-        ];
+        let want =
+            "testfilewithonelineshouldhaveonelineoutputnowthereisanotherlinesotwolinesoutput"
+                .to_string();
         assert_eq!(want, result);
     }
 
     #[test]
     fn parse_instances_of_mul_in_string_test_expect_one() {
         let string = "mul(2,4)extrawordsnothingmuch".to_string();
-        let result = parse_instances_of_mul_in_string(string);
+        let result = parse_instances_of_mul_in_string(&string);
         let result_length = result.len();
         let want = vec![(2, 4)];
         let want_length = 1;
@@ -86,7 +164,7 @@ mod tests {
     #[test]
     fn parse_instances_of_mul_in_string_test_expect_two() {
         let string = "mul(2,4)extrawordsmul(4,1)nothingmuch".to_string();
-        let result = parse_instances_of_mul_in_string(string);
+        let result = parse_instances_of_mul_in_string(&string);
         let result_length = result.len();
         let want = vec![(2, 4), (4, 1)];
         let want_length = 2;
@@ -97,7 +175,7 @@ mod tests {
     #[test]
     fn parse_instances_of_mul_in_string_test_expect_two_with_interference() {
         let string = "mul(2,4)extramul(2,2wordsmul(4,1)nothingmuch".to_string();
-        let result = parse_instances_of_mul_in_string(string);
+        let result = parse_instances_of_mul_in_string(&string);
         let result_length = result.len();
         let want = vec![(2, 4), (4, 1)];
         let want_length = 2;
@@ -109,7 +187,7 @@ mod tests {
     fn parse_instances_of_mul_in_string_test_example() {
         let string =
             "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))".to_string();
-        let result = parse_instances_of_mul_in_string(string);
+        let result = parse_instances_of_mul_in_string(&string);
         let result_length = result.len();
         let want = vec![(2, 4), (5, 5), (11, 8), (8, 5)];
         let want_length = 4;
@@ -138,6 +216,58 @@ mod tests {
         let digits = vec![(2, 4), (5, 5), (11, 8), (8, 5)];
         let result = sum_products(&digits);
         let want = 161;
+        assert_eq!(want, result);
+    }
+
+    #[test]
+    fn capture_mul_and_prefix_test_expect_1() {
+        let string = "xmul(2,4)%&mul[3,7]!@^do_not_".to_string();
+        let result = capture_mul_and_prefix(&string);
+        let result_length = result.len();
+        let want = vec!["xmul(2,4)".to_string()];
+        let want_length = 1;
+        assert_eq!(want, result);
+        assert_eq!(want_length, result_length);
+    }
+
+    #[test]
+    fn capture_mul_and_prefix_test_expect_2() {
+        let string = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)".to_string();
+        let result = capture_mul_and_prefix(&string);
+        let result_length = result.len();
+        let want = vec![
+            "xmul(2,4)".to_string(),
+            "%&mul[3,7]!@^do_not_mul(5,5)".to_string(),
+        ];
+        let want_length = 2;
+        assert_eq!(want, result);
+        assert_eq!(want_length, result_length);
+    }
+
+    #[test]
+    fn capture_do_or_dont_test_expect_2() {
+        let string = "%&mul[3,7]!@^do()_notdon't()_mul(5,5)".to_string();
+        let result = capture_do_or_dont(&string);
+        let result_length = result.len();
+        let want = vec!["do()".to_string(), "don't()".to_string()];
+        let want_length = 2;
+        assert_eq!(want, result);
+        assert_eq!(want_length, result_length);
+    }
+
+    #[test]
+    fn last_prefix_is_do_test_expect_true() {
+        let strings = vec!["don't()".to_string(), "do()".to_string()];
+        let result = last_prefix_is_do(&strings);
+        let want = true;
+        assert_eq!(want, result);
+    }
+
+    #[test]
+    fn last_prefix_is_do_test_expect_false() {
+        let strings = vec!["do()".to_string(), "don't()".to_string()];
+        let result = last_prefix_is_do(&strings);
+        let want = false;
         assert_eq!(want, result);
     }
 }
