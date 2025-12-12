b1845064847edc01ba096980ab5cbde9a4f4c8b7
Matt <mail@matthewtapps.com>
2025-12-13 08:25:37 +1100

    Day 3 part two

--- 2025/day-three/src/main.rs
+++ 2025/day-three/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     path::Path,
 };
 
-const INPUT: &str = "input.txt";
+const INPUT: &str = "mocks/mock1.txt";
 
 fn parse_file(file_path: impl AsRef<Path>) -> Vec<String> {
     let contents = File::open(file_path).expect("no such file");
@@ -16,40 +16,43 @@ fn parse_file(file_path: impl AsRef<Path>) -> Vec<String> {
 
 fn main() {
     let inputs = parse_file(INPUT);
-    let mut sum = 0;
+    let mut sum: i64 = 0;
 
     for line in inputs {
-        let mut left = 0;
-        let mut right = 0;
-        let length = line.len();
-        for index in 0..length {
-            let digit = line
-                .chars()
-                .nth(index)
-                .expect("couldn't get char index")
-                .to_string()
-                .parse()
-                .expect("couldn't parse char");
+        // Turn the line into a vec of u32s
+        let digits: Vec<u32> = line.chars().filter_map(|c| c.to_digit(10)).collect();
 
-            // Replace the left digit with the new highest, if we have at
-            // least one more digit remaining to take the place of the right digit
-            if digit > left && index < length - 1 {
-                left = digit;
-                right = 0;
-                continue;
+        let length = digits.len();
+        // Result will be a 12-digit array of u32s
+        let mut result = [0u32; 12];
+        // Start looking at index 0
+        let mut search_start = 0;
+
+        // current is the index of the result digit we're currently trying to fill
+        for current in 0..12 {
+            // We need at least 12 digits in a row
+            let search_end = length - (11 - current);
+
+            let mut best_value = 0;
+            let mut best_index = search_start;
+
+            for i in search_start..search_end {
+                // If the current digit is larger than our current best value,
+                // update the best value and best index
+                if digits[i] > best_value {
+                    best_value = digits[i];
+                    best_index = i;
+                }
             }
 
-            // Take the highest possible right digit
-            if digit > right {
-                right = digit;
-            }
+            // Set our result for the current index to the best value we found,
+            // and update our search start to be the index after our best index
+            result[current] = best_value;
+            search_start = best_index + 1;
         }
 
-        sum += (left.to_string() + &right.to_string()).parse::<i32>().expect("couldn't parse final number");
-
-        println!("{} {}", left, right);
+        sum += result.iter().fold(0i64, |acc, &d| acc * 10 + d as i64);
     }
 
     println!("{}", sum);
 }
-
